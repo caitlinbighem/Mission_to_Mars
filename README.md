83d82df45add986cd9d6b58974eb5638610cedc6
Bighem (US) <caitlin.a.bighem@boeing.com>
2022-08-10 14:06:39 -0500

    Mission to Mars Challenge Final Upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Mission_to_Mars
+
+## Overview 
+The purpose of this assignment is to scrape the data from several websites to create a webpage, which highlights data on the planet Mars. 
+
+###Data Sources
+https://redplanetscience.com
+https://spaceimages-mars.com
+https://galaxyfacts-mars.com
+https://marshemisphere.com
+
+* **Tools:** Pandas, Splinter, BeautifulSoup, Jupyter Notebook
+* **Applications:** MongoDB, Flask
+* **Languages:** Python, HTML, CSS
+
+###Summary
+In this project, there is an automated web browser that visits the NASA news website and the Mars Hemispheres website to scrape data for the Mission to Mars in index.html. It is stored in a NoSQL database in scraping.py, and then render the data in a web application created with Flask in app.py. 
--- scraping.py
+++ scraping.py
@@ -21,7 +21,7 @@ def scrape_all():
  
     news_title, news_paragraph= mars_news(browser)
 
-    hemisphere_image_urls=hemisphere(browser)
+    hemisphere_image_urls=hemispheres(browser)
     # Run all scraping functions and store results in dictionary 
     data={
         "news_title": news_title,
@@ -42,13 +42,14 @@ def scrape_all():
 def mars_news(browser):
 
     # visit NASA website 
-    url= 'https://mars.nasa.gov/news/'
+    url= 'https://redplanetscience.com/'
     browser.visit(url)
 
     #Optional delay for website 
     # Here we are searching for elements with a specific combination of tag (ul) and (li) and attriobute (item_lit) and (slide)
     # Ex. being <ul class= "item_list">
-    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    # browser.is_element_present_by_css("ul.item_list li.slide", wait_time=1)
+    browser.is_element_present_by_css("div.list_text", wait_time=1)
 
     # HTML Parser. Convert the brpwser html to a soup object and then quit the browser
     html= browser.html 
@@ -58,7 +59,8 @@ def mars_news(browser):
     try:
         #slide_elem looks for <ul /> tags and descendents <li />
         # the period(.) is used for selecting classes such as item_list
-        slide_elem= news_soup.select_one('ul.item_list li.slide')
+        # slide_elem= news_soup.select_one('ul.item_list li.slide')
+        slide_elem= news_soup.select_one('div.list_text')
 
         # Chained the (.find) to slide_elem which says this variable holds lots of info, so look inside to find this specific entity
         # Get Title
@@ -77,20 +79,21 @@ def mars_news(browser):
 def featured_image(browser):
 
     # Visit URL 
-    url= 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mar'
+    # url= 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mar'
+    url = 'https://spaceimages-mars.com'
     browser.visit(url)
 
     # Find and click the full_image button
-    full_image_elem= browser.find_by_id('full_image')[0]
+    full_image_elem= browser.find_by_tag('button')[1]
     full_image_elem.click()
 
-    # Find the more info button and click that 
-    # is_element_present_by_text() method to search for an element that has the provided text
-    browser.is_element_present_by_text('more info', wait_time=1)
+    # # Find the more info button and click that 
+    # # is_element_present_by_text() method to search for an element that has the provided text
+    # browser.is_element_present_by_text('more info', wait_time=1)
 
-    # will take our string 'more info' and add link associated with it, then click
-    more_info_elem=browser.links.find_by_partial_text('more info')
-    more_info_elem.click()
+    # # will take our string 'more info' and add link associated with it, then click
+    # more_info_elem=browser.links.find_by_partial_text('more info')
+    # more_info_elem.click()
 
     # Parse the resulting html with soup
     html=browser.html
@@ -104,12 +107,12 @@ def featured_image(browser):
         # the .get('src') pulls the link to the image
 
         # WE are telling soup to go to figure tag, then within that look for an 'a' tag then within that look for a 'img' tag
-        img_url_rel= img_soup.select_one('figure.lede a img').get("src")
+        img_url_rel= img_soup.find('img', class_="fancybox-image").get("src")
     
     except AttributeError:
         return None
     # Need to get the FULL URL: Only had relative path before
-    img_url= f'https://www.jpl.nasa.gov{img_url_rel}'
+    img_url= f'https://spaceimages-mars.com{img_url_rel}'
 
     return img_url
 
@@ -121,14 +124,14 @@ def mars_facts():
     # Add try/except for error handling
     try:
         # Creating DF by telling function to look for first html table in site it encounters by indexing it to zero
-        df=pd.read_html('http://space-facts.com/mars/')[0]
+        df=pd.read_html('https://galaxyfacts-mars.com')[0]
 
     # BaseException, catches multiple types of errors
     except BaseException:
         return None
     
     # Assigning columns, and set 'description' as index 
-    df.columns=['description', 'value']
+    df.columns=['description', 'Mars', 'Earth']
     df.set_index('description', inplace=True)
 
     #Convert back to HTML format, add bootstrap
@@ -136,34 +139,75 @@ def mars_facts():
 
 ## > SCRAPE HEMISPHERE <
 
-def hemisphere(browser):
-    url='https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
-    browser.visit(url)
+# def hemisphere(browser):
+#     url='https://marshemispheres.com/'
+#     browser.visit(url)
 
 
+#     hemisphere_image_urls = []
+
+#     imgs_links= browser.find_by_css("a.product-item h3")
+
+#     for x in range(4):
+#         # hemisphere={}
+
+#         # Find elements going to click link 
+#         browser.find_by_css("a.product-item img")[x].click()
+#         hemidata=scrape_hemisphere(browsehtml)
+
+#         # Find sample Image link
+#         sample_img= browser.find_link_by_text("Sample").first
+#         hemisphere['img_url']=sample_img['href']
+
+#         # Get hemisphere Title
+#         hemisphere['title']=browser.find_by_css("h2.title").text
+
+#         # Add Objects to hemisphere_img_urls list
+#         hemisphere_image_urls.append(hemisphere)
+
+#         # Go Back
+#         browser.back()
+#     return hemisphere_image_urls
+
+def hemispheres(browser):
+    url = 'https://marshemispheres.com/'
+
+    browser.visit(url + 'index.html')
+
+    # Click the link, find the sample anchor, return the href
     hemisphere_image_urls = []
-
-    imgs_links= browser.find_by_css("a.product-item h3")
-
-    for x in range(len(imgs_links)):
-        hemisphere={}
-
-        # Find elements going to click link 
-        browser.find_by_css("a.product-item h3")[x].click()
-
-        # Find sample Image link
-        sample_img= browser.find_link_by_text("Sample").first
-        hemisphere['img_url']=sample_img['href']
-
-        # Get hemisphere Title
-        hemisphere['title']=browser.find_by_css("h2.title").text
-
-        #Add Objects to hemisphere_img_urls list
-        hemisphere_image_urls.append(hemisphere)
-
-        # Go Back
+    for i in range(4):
+        # Find the elements on each loop to avoid a stale element exception
+        browser.find_by_css("a.product-item img")[i].click()
+        hemi_data = scrape_hemisphere(browser.html)
+        hemi_data['img_url'] = url + hemi_data['img_url']
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemi_data)
+        # Finally, we navigate backwards
         browser.back()
+
     return hemisphere_image_urls
+
+def scrape_hemisphere(html_text):
+    # parse html text
+    hemi_soup = soup(html_text, "html.parser")
+
+    # adding try/except for error handling
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+
+    except AttributeError:
+        # Image error will return None, for better front-end handling
+        title_elem = None
+        sample_elem = None
+
+    hemispheres = {
+        "title": title_elem,
+        "img_url": sample_elem
+    }
+
+    return hemispheres
 
 if __name__== "__main__":
     # If running as script, print scrapped data
